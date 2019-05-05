4b94a04ee9b4aca8114a8d7757a8843eaeb5c59a
annin401 <cando85@icloud.com>
2019-05-05 14:37:36 +0900

    create SoundPlayer

---
+++ SoundPlayer.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from mutagen.mp3 import MP3 as mp3
+import pygame
+
+class SoundPlayer():
+
+    def __init__(self):
+        self.filename = 'Resources/chime_sound.mp3'
+        self.__play_music_times = -1 # infinite loop
+        pygame.mixer.init()
+        pygame.mixer.music.load(self.filename)
+        pygame.mixer.music.set_volume(1.0)
+        
+    def start(self):
+        pygame.mixer.music.play(self.__play_music_times)
+
+    def stop(self):
+        pygame.mixer.music.stop()
